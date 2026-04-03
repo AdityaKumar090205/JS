2daaf18a12b9d154a716478cc2087361558e5d97
AdityaKumar090205 <aviadikumar@gmail.com>
2026-04-03 14:02:25 +0000

    Javascript basics

---
+++ 01_basicsjs/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+console.log(3+3)
+
+console.log("Aditya")
+
+let name="aditya"
+let age=21
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
+
+//
