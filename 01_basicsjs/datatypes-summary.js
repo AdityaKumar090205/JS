7bc4ad2667944efcc9a9982f52e935f9eee8229d
AdityaKumar090205 <aviadikumar@gmail.com>
2026-04-07 05:15:39 +0000

    datatypes summary

---
+++ 01_basicsjs/datatypes-summary.js
@@ -0,0 +1,33 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 30
+const scoreValue =30.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let accountEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const BigNumber = 3685824619747474n
+
+// Reference(Non-Primitive)
+
+//Array, Objects, Functions
+
+const heroes=["spiderman","batman","ironman"];
+let myObj = {
+    name: "aditya",
+    age: 21,
+}
+
+const Myfunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myObj);
