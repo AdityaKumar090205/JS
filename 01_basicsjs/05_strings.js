96dd7d993b72c8fc53190319f9810141144c626c
AdityaKumar090205 <aviadikumar@gmail.com>
2026-04-08 07:28:35 +0000

    string operations

---
+++ 01_basicsjs/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Aditya"
+const repoCount = 13
+
+//console.log(name + repoCount + " Value")
+
+//String Interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("ak-gamer-09")
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('g'));
+
+//const newString = gameName.substring(0,5)
+//console.log(newString);
+
+//const anotherString = gameName.slice(-8,4)
+//console.log(anotherString);
+
+const newStringOne = " aokrfv "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https//aditya.com//ak%30adityakumar.com"
+console.log(url.replace('%30','&$'));
+console.log(url.includes('yumar'));
+
+console.log(gameName.split('-'));
