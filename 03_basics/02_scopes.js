e5ec5ed2f4d9410d8762ff8446280e3b85da23aa
AdityaKumar090205 <aviadikumar@gmail.com>
2026-04-13 15:17:22 +0000

    scopes basics

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,50 @@
+// var c = 300
+let a = 100
+if(true){
+    let a = 10
+    const b = 20
+    // console.log("INNER: ",a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "aditya"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "aditya"
+    if(username === "aditya"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++++++++Interesting+++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num+1
+}
+
+addone(5)
+
+const addTwo = function(num){
+    return num+2
+}
+
+addTwo(5)
